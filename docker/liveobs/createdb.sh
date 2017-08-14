f2b542ea47a7e57021ad6fc7c35cbd52faa2645f
Colin Wren <colin.wren@bjss.com>
2017-08-14 17:09:04 +0100

    Adding ability to generate client config DB via makefile

--- docker/liveobs/createdb.sh
+++ docker/liveobs/createdb.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+until $(nc -z $HOST 5432); do
+  echo "Waiting for PostgreSQL to come up"
+  sleep 1
+done
 /entrypoint.sh openerp-server &
-echo "sleeping for 20 seconds..."
-sleep 20
+until $(curl --output /dev/null --silent --head --fail http://localhost:8069/web); do
+  echo "Waiting for Odoo to come up"
+  sleep 5
+done
 echo -e "db_host = $HOST\ndb_user = $USER\ndb_password = $PASSWORD" >> /etc/odoo/server.cfg
 source /opt/nh/venv/bin/activate
 cd /opt/odoo/db_init
 inv demo.slam -d liveobs -c /etc/odoo/server.cfg
+inv test.test_enable_all_modules -d liveobs
+export PGUSER=${USER}
+export PGPASSWORD=${PASSWORD}
+pg_dump -h ${HOST} -d liveobs > /opt/dumps/liveobs.sql
