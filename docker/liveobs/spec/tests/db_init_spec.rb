b95f6a61caa9a5f89a5eba451f63ddc7262f258f
Colin Wren <colin.wren@bjss.com>
2017-08-15 13:21:06 +0100

    Sorting out failing test

--- docker/liveobs/spec/tests/db_init_spec.rb
+++ docker/liveobs/spec/tests/db_init_spec.rb
@@ -11,11 +11,6 @@ describe "LiveObs Docker image - Database Initialisation" do
     it{ should be_directory }
   end
 
-  describe file('/opt/odoo/createdb.sh') do
-    it{ should exist }
-    it{ should be_executable }
-  end
-
   describe file('/opt/odoo/db_init/demo/demo.py') do
     it{ should exist }
   end
