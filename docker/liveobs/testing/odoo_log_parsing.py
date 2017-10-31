101a1388f00090d5f8ef47f432b7dfa9a9366f16
Colin Wren <colin.wren@bjss.com>
2017-10-31 15:08:29 +0000

    Getting coverage collection going

--- docker/liveobs/testing/odoo_log_parsing.py
+++ docker/liveobs/testing/odoo_log_parsing.py
@@ -2,20 +2,35 @@ import sys, subprocess, time
 
 failing_tests = []
 
-for line in sys.stdin:
+while 1:
+
+    try:
+        line = sys.stdin.readline()
+        if 'FAIL:' in line:
+            failing_tests.append(line)
+        # if 'openerp.modules.loading: All post-tested' in line:
+        if 'Initiating shutdown' in line:
+            # find_file = subprocess.Popen(["docker", "exec", "-u", "odoo", "liveobs_web_1", "ls", "/opt/nh"])
+            # coverage = subprocess.Popen(["docker", "exec", "-u", "odoo", "liveobs_web_1", "/opt/nh/venv/bin/coverage", "xml", "-o", "/opt/nh/unit_test_coverage.xml"])
+            # for cov_out, cov_err in coverage.commnicate():
+            #     sys.stdout.write(cov_out)
+            # subprocess.Popen(["docker", "exec", "web", "collect-coverage"], stdout=subprocess.PIPE)
+            # collect = subprocess.Popen(["docker", "cp", "web:/opt/nh/unit_test_coverage.xml", "unit_test_coverage"], stdout=subprocess.PIPE)
+            # sys.stdout.write(collect.communicate())
+            # stop = subprocess.Popen(["docker-compose", "stop", "web"], stdout=subprocess.PIPE)
+            # sys.stdout.write(stop.communicate())
+            time.sleep(2)
+            if failing_tests:
+                sys.stdout.write('--------------------FAILING TESTS--------------------------\n')
+                for test in failing_tests:
+                    sys.stdout.write(' - {}\n'.format(test))
+                sys.exit(1)
+            else:
+                sys.exit(0)
+    except KeyboardInterrupt:
+        break
+
+    if not line:
+        break
+
     sys.stdout.write(line)
-    if 'FAIL:' in line:
-        failing_tests.append(line)
-    if 'Initiating shutdown' in line:
-        subprocess.Popen(["docker", "exec", "liveobs_web_1", "/opt/nh/venv/bin/coverage", "xml", "-o", "/opt/nh/unit_test_coverage.xml"], stdout=subprocess.PIPE)
-        # subprocess.Popen(["docker", "exec", "web", "collect-coverage"], stdout=subprocess.PIPE)
-        subprocess.Popen(["docker", "cp", "web:/opt/nh/unit_test_coverage.xml", "unit_test_coverage"], stdout=subprocess.PIPE)
-        subprocess.Popen(["docker-compose", "stop", "web"], stdout=subprocess.PIPE)
-        time.sleep(2)
-        if failing_tests:
-            sys.stdout.write('--------------------FAILING TESTS--------------------------\n')
-            for test in failing_tests:
-                sys.stdout.write(' - {}\n'.format(test))
-            sys.exit(1)
-        else:
-            sys.exit(0)
