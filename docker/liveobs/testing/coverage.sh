7869a8e66e550dfe917fb7c4247ff941768499f2
Colin Wren <colin.wren@bjss.com>
2017-10-31 16:22:06 +0000

    Moving coverage to liveobs-build and not liveobs-provisioning

--- docker/liveobs/testing/coverage.sh
+++ docker/liveobs/testing/coverage.sh
@@ -2,4 +2,4 @@
 echo -e "db_host = $HOST\ndb_user = $USER\ndb_password = $PASSWORD" >> /etc/odoo/server.cfg
 /entrypoint.sh coverage -d db -u nh_eobs_slam --test-enable --stop-after-init
 /entrypoint.sh collect-coverage
-s3cmd put /opt/nh/unit_test_coverage.xml "s3://liveobs-provisioning-eu-west-1/artifacts/odoo/coverage/${GO_PIPELINE_COUNTER}/unit_test_coverage.xml"
+s3cmd put /opt/nh/unit_test_coverage.xml "s3://liveobs-build/artifacts/odoo/coverage/${GO_PIPELINE_COUNTER}/unit_test_coverage.xml"
