eff175b2a5305bebbc6aca1d3958882afca974ec
Colin Wren <colin.wren@bjss.com>
2017-11-01 11:43:56 +0000

    - Getting coverage data from S3

--- docker/liveobs/testing/coverage.sh
+++ docker/liveobs/testing/coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e "db_host = $HOST\ndb_user = $USER\ndb_password = $PASSWORD" >> /etc/odoo/server.cfg
-/entrypoint.sh coverage -d db -u nh_eobs_slam --test-enable --stop-after-init
+/entrypoint.sh coverage -d db -u nh_odoo_fixes --test-enable --stop-after-init
 /entrypoint.sh collect-coverage
 s3cmd put /opt/nh/unit_test_coverage.xml "s3://liveobs-provisioning-eu-west-1/artifacts/odoo/coverage/${GO_PIPELINE_COUNTER}/unit_test_coverage.xml"
