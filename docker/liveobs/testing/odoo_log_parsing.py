e6be07f7958abbff7da16d23203baac78646d5d0
Colin Wren <colin.wren@bjss.com>
2017-11-09 14:34:52 +0000

    Got Xunit report running locally

--- docker/liveobs/testing/odoo_log_parsing.py
+++ docker/liveobs/testing/odoo_log_parsing.py
@@ -1,11 +1,11 @@
 import sys, subprocess, time, re
 from xunitgen import XunitDestination, EventReceiver, toxml
 
-destination = XunitDestination('/opt/odoo')
-receiver = xunitgen.EventReceiver()
-TEST_START_REGEX = re.compile(r"\.* openerp\.modules\.module: (.*) running tests\.")
-TEST_CASE_REGEX = re.compile(r"\.*: ([a-zA-z_]+) \(([a-zA-z\._]+)\)")
-TEST_END_REGEX = re.compile(r"\.*([a-z\._]+): Ran (\d+) tests in ([0-9\.]+)")
+destination = XunitDestination('.')
+receiver = EventReceiver()
+TEST_START_REGEX = re.compile(r".* openerp\.modules\.module: (.*) running tests\.")
+TEST_CASE_REGEX = re.compile(r".*: ([a-zA-z_]+) \(([a-zA-z\._]+)\)")
+TEST_END_REGEX = re.compile(r".* (.+): Ran (\d+) test(s)? in ([0-9\.]+)")
 failing_tests = []
 
 while 1:
@@ -15,15 +15,14 @@ while 1:
         if 'running tests' in line:
             test_name = TEST_START_REGEX.match(line).groups()[0]
             test_location = test_name.replace('openerp.addons.', '').replace('.', '/')
-            receiver.being_case(test_name, 0, test_location)
-            sys.stdout.write("Began test case")
+            receiver.begin_case(test_name, 0, test_location)
         if 'Ran' in line and 'tests' in line:
+            sys.stdout.write("LINE: {}".format(line))
             line_match = TEST_END_REGEX.match(line).groups()
             test_name = line_match[0]
             number_of_tests = line_match[1]
-            test_time = float(line_match[2])
+            test_time = float(line_match[3])
             receiver.end_case(test_name, test_time)
-            sys.stdout.write("Ended test case")
         if 'FAIL:' in line:
             failing_tests.append(line)
             receiver.failure('', '')
@@ -31,7 +30,7 @@ while 1:
         if 'Initiating shutdown' in line:
             time.sleep(2)
             test_results = receiver.results()
-            destination.write_reports('/opt/nh/unit_test_results.xml', 'testsuite', test_results)
+            destination.write_reports('unit_test_results', 'testsuite', test_results)
             if failing_tests:
                 sys.stdout.write('--------------------FAILING TESTS--------------------------\n')
                 for test in failing_tests:
