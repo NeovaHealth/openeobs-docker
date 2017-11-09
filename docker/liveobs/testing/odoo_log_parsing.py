ba3b6e311b3906b9e73a14572da31a9b13846531
Colin Wren <colin.wren@bjss.com>
2017-11-09 10:16:54 +0000

    Starting to add xunitgen to `odoo_log_parsing.py` so can get Xunit reports

--- docker/liveobs/testing/odoo_log_parsing.py
+++ docker/liveobs/testing/odoo_log_parsing.py
@@ -1,25 +1,37 @@
-import sys, subprocess, time
+import sys, subprocess, time, re
+from xunitgen import XunitDestination, EventReceiver, toxml
 
+destination = XunitDestination('/opt/odoo')
+receiver = xunitgen.EventReceiver()
+TEST_START_REGEX = re.compile(r"\.* openerp\.modules\.module: (.*) running tests\.")
+TEST_CASE_REGEX = re.compile(r"\.*: ([a-zA-z_]+) \(([a-zA-z\._]+)\)")
+TEST_END_REGEX = re.compile(r"\.*([a-z\._]+): Ran (\d+) tests in ([0-9\.]+)")
 failing_tests = []
 
 while 1:
 
     try:
         line = sys.stdin.readline()
+        if 'running tests' in line:
+            test_name = TEST_START_REGEX.match(line).groups()[0]
+            test_location = test_name.replace('openerp.addons.', '').replace('.', '/')
+            receiver.being_case(test_name, 0, test_location)
+            sys.stdout.write("Began test case")
+        if 'Ran' in line and 'tests' in line:
+            line_match = TEST_END_REGEX.match(line).groups()
+            test_name = line_match[0]
+            number_of_tests = line_match[1]
+            test_time = float(line_match[2])
+            receiver.end_case(test_name, test_time)
+            sys.stdout.write("Ended test case")
         if 'FAIL:' in line:
             failing_tests.append(line)
+            receiver.failure('', '')
         # if 'openerp.modules.loading: All post-tested' in line:
         if 'Initiating shutdown' in line:
-            # find_file = subprocess.Popen(["docker", "exec", "-u", "odoo", "liveobs_web_1", "ls", "/opt/nh"])
-            # coverage = subprocess.Popen(["docker", "exec", "-u", "odoo", "liveobs_web_1", "/opt/nh/venv/bin/coverage", "xml", "-o", "/opt/nh/unit_test_coverage.xml"])
-            # for cov_out, cov_err in coverage.commnicate():
-            #     sys.stdout.write(cov_out)
-            # subprocess.Popen(["docker", "exec", "web", "collect-coverage"], stdout=subprocess.PIPE)
-            # collect = subprocess.Popen(["docker", "cp", "web:/opt/nh/unit_test_coverage.xml", "unit_test_coverage"], stdout=subprocess.PIPE)
-            # sys.stdout.write(collect.communicate())
-            # stop = subprocess.Popen(["docker-compose", "stop", "web"], stdout=subprocess.PIPE)
-            # sys.stdout.write(stop.communicate())
             time.sleep(2)
+            test_results = receiver.results()
+            destination.write_reports('/opt/nh/unit_test_results.xml', 'testsuite', test_results)
             if failing_tests:
                 sys.stdout.write('--------------------FAILING TESTS--------------------------\n')
                 for test in failing_tests:
